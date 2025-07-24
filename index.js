3dba30ab038432116dd2fa6b79b0902941252d55
PradeepGorkhali <adhikaripradeep11@gmail.com>
2025-07-24 20:52:57 +0545

    first cm

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use("/api/v1/message", messageRoute);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist"));
 })
 
 
