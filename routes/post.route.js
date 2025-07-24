df6017e6963748aea9d7989dd4c399a0985b66df
PradeepGorkhali <adhikaripradeep11@gmail.com>
2025-07-24 21:39:58 +0545

    firs

--- routes/post.route.js
+++ routes/post.route.js
@@ -5,9 +5,9 @@ import { addComment, addNewPost, bookmarkPost, deletePost, dislikePost, getAllPo
 
 const router = express.Router();
 
-router.route("/addpost").post(isAuthenticated, upload.single('image'), addNewPost);
-router.route("/all").get(isAuthenticated,getAllPost);
-router.route("/userpost/all").get(isAuthenticated, getUserPost);
+router.route("/addpost").post(upload.single('image'), addNewPost);
+router.route("/all").get(getAllPost);
+router.route("/userpost/all").get(getUserPost);
 router.route("/:id/like").get(isAuthenticated, likePost);
 router.route("/:id/dislike").get(isAuthenticated, dislikePost);
 router.route("/:id/comment").post(isAuthenticated, addComment); 
